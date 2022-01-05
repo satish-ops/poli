4eeb8d0b7957ff2504eff8fa9f9eaad31bb28ad3
satish-opss <63900535+satish-ops@users.noreply.github.com>
2022-01-05 12:25:14 +0530

    Update upload.bat

--- upload.bat
+++ upload.bat
@@ -1,1 +1,2 @@
-curl.exe -v -u admin:admin --upload-file police2  http://19.199.136.133:8081/repository/come-on/
+cp hello.sh hello1.sh
+curl.exe -v -u admin:admin --upload-file hello1.sh http://19.199.136.133:8081/repository/come-on/
