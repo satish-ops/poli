c2b1922ed795400b83a7d8d7f9db8b4d88b789b8
satish-opss <63900535+satish-ops@users.noreply.github.com>
2022-01-10 12:18:55 +0530

    Update and rename upload.bat to download.bat

---
+++ download.bat
@@ -0,0 +1,1 @@
+curl -X GET -u admin:admin http://localhost:8081/repository/sample/com/hey/maven-metadata.xml -O
--- upload.bat
+++
@@ -1,2 +0,0 @@
-copy hello.sh hello1.sh
-curl.exe -v -u admin:admin --upload-file hello1.sh http://19.199.136.133:8081/repository/come-on/
